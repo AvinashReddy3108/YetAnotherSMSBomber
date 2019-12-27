c4173d5d921624f2f0d5c341500da75fa84f7d5b
skp121 <58383473+skp121@users.noreply.github.com>
2019-12-27 12:36:27 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -12,7 +12,7 @@ from Provider import Provider
 parser = argparse.ArgumentParser()
 parser.add_argument('target', metavar='TARGET', type=lambda value: (_ for _ in ()).throw(argparse.ArgumentTypeError(f'{value} is an invalid mobile number')) if len(value) != 10 else value,
                     help='Target mobile number without country code')
-parser.add_argument('--sms', '-S', type=int
+parser.add_argument('--sms', '-S', type=int,
                     help='Number of sms to target (default: 50000)', default=50000)
 parser.add_argument('--country', '-c', type=int,
                     help='Country code without (+) sign (default: 91)', default=91)
