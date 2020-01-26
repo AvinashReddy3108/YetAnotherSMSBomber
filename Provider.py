9f70e65621b56a42fd9b826ec8f5220d0ce0f5fe
Avinash Reddy <cassilasreddythemostwanted3108@gmail.com>
2020-01-26 10:55:58 +0530

    Updated default user agent string.

--- Provider.py
+++ Provider.py
@@ -21,7 +21,7 @@ class Provider:
     def _headers(self):
         tmp_headers = {
             "User-Agent":
-            "Mozilla/5.0 (X11; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0"
+            "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0"
         }
         if 'headers' in self.config:
             if "Content-Length" in self.config['headers'].keys():
